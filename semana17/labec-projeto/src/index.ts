3aa80b720d685f83344df0168e0955032330f1d8
mhsouza88 <88038506+mhsouza88@users.noreply.github.com>
2021-12-03 21:30:20 -0300

    Finalizando

--- semana17/labec-projeto/src/index.ts
+++ semana17/labec-projeto/src/index.ts
@@ -1,20 +1,33 @@
-import express from "express"
+import express, { Express, Request, Response} from "express"
 import cors from "cors"
 import { AddressInfo } from "net"
-import { getAllRecipes } from "./endpoints/getAllRecipes"
+import { 
+   getCreateUser, 
+   getAllUsers, 
+   getCreateProduct, 
+   getAllProducts, 
+   getRegisterPurchase, 
+   getUserPurchases } from "./endpoints/allEndpoints"
 
-export const app = express()
+export const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/recipes", getAllRecipes)
-
+// Endpoints
+app.post("/users", getCreateUser) // Criar usuário
+app.get("/users", getAllUsers) // Busca todos os usuários
+app.post("/products", getCreateProduct) // Cadastro um novo produto
+app.get("/products", getAllProducts) // Busca todos os produtos
+app.post("/purchases", getRegisterPurchase) // Registra compra
+app.get("/users/:user_id/purchases", getUserPurchases) // Busca as compras de um usuário
+ 
+// Servidor
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
       const address = server.address() as AddressInfo;
-      console.log(`Server is running in http://localhost:${address.port}`);
+      console.log(`Servidor ativo em http://localhost:${address.port}`)
    } else {
-      console.error(`Failure upon starting server.`);
+      console.error("Houve uma falha ao iniciar o servidor.")
    }
 })
